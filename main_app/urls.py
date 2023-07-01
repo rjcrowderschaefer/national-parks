642f6dfd16e6445d8848db3cfc66d0c8cec8a73a
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-01 11:40:26 -0400

    base.html template file built with nav

---
+++ main_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
+    # path('states/', views.StateList.as_view(), name="state_list"),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(View):
+    def get(self, request):
+        return HttpResponse("About Page")
+    
+# class State:
+#     def __init__(self, name, image, description):
+#         self.name = name
+#         self.image = image
+#         self.description = description
